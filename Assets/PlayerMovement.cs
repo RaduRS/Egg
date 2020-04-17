fe68dfb620e6bf0a076bbe46572bc076eac47de8
RaduRS <rsrusu90@gmail.com>
2020-04-17 16:42:48 +0100

    add project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sideForce = 500f;
+    public bool keyPressedRight = false;
+    public bool keyPressedLeft = false;
+
+    private void Update()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            keyPressedRight = true;
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            keyPressedLeft = true;
+        }
+    }
+
+    //adding force
+    private void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (keyPressedRight == true)
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+            keyPressedRight = false;
+        }
+        else if (keyPressedLeft == true)
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+            keyPressedLeft = false;
+        }
+    }
+}
